354af51d61ca01ece3bc88c04088f292d93d3387
direcctorcn <directorcn@163.com>
2021-03-30 17:11:03 +0800

    feat: update map

--- docs/data/API.js
+++ docs/data/API.js
@@ -2,5 +2,95 @@ const API = {
   id: 'FrontEndMap|API',
   name: 'API',
   path: '',
-  children: []
+  children: [
+    {
+      id: 'FrontEndMap|API|Browser',
+      name: 'Browser',
+      children: [
+        {
+          id: 'FrontEndMap|API|Browser|DOM',
+          name: 'DOM',
+          children: [
+            {
+              id: 'FrontEndMap|API|Browser|DOM|Nodes',
+              name: 'Nodes',
+              children: []
+            },
+            {
+              id: 'FrontEndMap|API|Browser|DOM|Ranges',
+              name: 'Ranges',
+              children: []
+            },
+            {
+              id: 'FrontEndMap|API|Browser|DOM|Events',
+              name: 'Events',
+              children: []
+            },
+          ]
+        },
+        {
+          id: 'FrontEndMap|API|Browser|BOM',
+          name: 'BOM',
+          children: [
+            {
+              id: 'FrontEndMap|API|Browser|BOM|window',
+              name: 'window',
+              children: [
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|setInterval',
+                  name: 'setInterval',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|setTimeout',
+                  name: 'setTimeout',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|Location',
+                  name: 'Location',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|History',
+                  name: 'History',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|Screen',
+                  name: 'Screen',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|LocalStorage',
+                  name: 'LocalStorage',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|SessionStorage',
+                  name: 'SessionStorage',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|API|Browser|BOM|window|cookie',
+                  name: 'cookie',
+                  children: []
+                },
+              ]
+            }
+          ]
+        },
+      ]
+    },
+    {
+      id: 'FrontEndMap|API|Node',
+      name: 'Node',
+      children: []
+    },
+    {
+      id: 'FrontEndMap|API|other',
+      name: 'others',
+      children: []
+    },
+  ]
 }
--- docs/data/CSS.js
+++ docs/data/CSS.js
@@ -2,5 +2,221 @@ const CSS = {
   id: 'FrontEndMap|CSS',
   name: 'CSS',
   path: '',
-  children: []
+  children: [
+    {
+      id: 'FrontEndMap|CSS|语法/词法',
+      name: '语法/词法',
+      children: []
+    },
+    {
+      id: 'FrontEndMap|CSS|@规则',
+      name: '@规则',
+      children: [
+        {
+          id: 'FrontEndMap|CSS|@规则|@charset',
+          name: '@charset',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@import',
+          name: '@import',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@media',
+          name: '@media',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@page',
+          name: '@page',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@counter-style',
+          name: '@counter-style',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@keyframes',
+          name: '@keyframes',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@font-face',
+          name: '@font-face',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@supports',
+          name: '@supports',
+        },
+        {
+          id: 'FrontEndMap|CSS|@规则|@namespace',
+          name: '@namespace',
+        }
+      ]
+    },
+    {
+      id: 'FrontEndMap|CSS|普通规则',
+      name: '普通规则',
+      children: [
+        {
+          id: 'FrontEndMap|CSS|普通规则|Selectors',
+          name: 'Selectors',
+          children: [
+            {
+              id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector',
+              name: 'simple selector',
+              children: [
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector|ID selector',
+                  name: 'ID selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector|classes selector',
+                  name: 'class selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector|pseudo-classes selector',
+                  name: 'pseudo-classes selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector|Attribute selector',
+                  name: 'attribute selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector|Type selector',
+                  name: 'type selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector|pseudo-elements selector',
+                  name: 'pseudo-elements selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|simple selector|universal selector',
+                  name: 'universal selector',
+                  children: []
+                },
+              ]
+            },
+            {
+              id: 'FrontEndMap|CSS|普通规则|Selectors|compound selector',
+              name: 'compound selector',
+              children: []
+            },
+            {
+              id: 'FrontEndMap|CSS|普通规则|Selectors|complex selector',
+              name: 'complex selector',
+              children: [
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|complex selector|child selector',
+                  name: 'child selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|complex selector|next sibling selector',
+                  name: 'next sibling selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|complex selector|subsequent-sibling selector',
+                  name: 'subsequent-sibling selector',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Selectors|complex selector|列选择器',
+                  name: '列选择器',
+                  children: []
+                },
+              ]
+            },
+            {
+              id: 'FrontEndMap|CSS|普通规则|Selectors|selector list',
+              name: 'selector list',
+              children: []
+            },
+          ]
+        },
+        {
+          id: 'FrontEndMap|CSS|普通规则|Declaration',
+          name: 'Declaration',
+          children: [
+            {
+              id: 'FrontEndMap|CSS|普通规则|Declaration|Key',
+              name: 'Key',
+              children: [
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Declaration|Key|property',
+                  name: 'property',
+                  children: []
+                },
+                {
+                  id: 'FrontEndMap|CSS|普通规则|Declaration|Key|variable',
+                  name: 'variable',
+                  children: []
+                },
+              ]
+            },
+            {
+              id: 'FrontEndMap|CSS|普通规则|Declaration|Value',
+              name: 'Value',
+              children: []
+            }
+          ]
+        },
+      ]
+    },
+    {
+      id: 'FrontEndMap|CSS|机制',
+      name: '机制',
+      children: [
+        {
+          id: 'FrontEndMap|CSS|机制|选择器优先级',
+          name: '选择器优先级',
+          children: []
+        },
+        {
+          id: 'FrontEndMap|CSS|机制|伪类',
+          name: '伪类',
+          children: []
+        },
+        {
+          id: 'FrontEndMap|CSS|机制|伪元素',
+          name: '伪元素',
+          children: []
+        },
+        {
+          id: 'FrontEndMap|CSS|机制|动画',
+          name: '动画',
+          children: []
+        },
+        {
+          id: 'FrontEndMap|CSS|机制|排版',
+          name: '排版',
+          children: [
+            {
+              id: 'FrontEndMap|CSS|机制|排版|Normal flow',
+              name: 'Normal flow',
+              children: []
+            },
+            {
+              id: 'FrontEndMap|CSS|机制|排版|Flex',
+              name: 'Flex',
+              children: []
+            },
+            {
+              id: 'FrontEndMap|CSS|机制|排版|Grid',
+              name: 'Grid',
+              children: []
+            }
+          ]
+        },
+      ]
+    },
+    {
+      id: 'FrontEndMap|CSS|计算函数',
+      name: '计算函数',
+      children: []
+    }
+  ]
 }
